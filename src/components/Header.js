995d3ea542c4f86f5bef4a55128d595fc1f84d7b
zijies1 <546641464@qq.com>
2019-05-06 15:29:22 +1000

    add background to the home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,23 @@ export default class Header extends React.Component {
   // render
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-          <div className="container">
-              <a className="navbar-brand" href="#">LoopLAB</a>
-              <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
-                  <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav ml-auto">
-                      <li className="nav-item">
-                          <a className="nav-link" href="#">Home</a>
-                      </li>
-                      <li className="nav-item">
-                          <a className="nav-link" href="/login">Login</a>
-                      </li>
-                  </ul>
-              </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="main-nav">
+        <div className="container">
+          <a href="index.html" className="navbar-brand">Health Care</a>
+          <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="/" className="nav-link">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link">Login</a>
+              </li>
+            </ul>
           </div>
+        </div>
       </nav>
     );
   }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,78 +1,81 @@
 import React from "react";
 import style from "./style.css";
 import Header from "../Header";
+import backgroundImage1 from "../../../public/media/headerImage1.jpg";
+import backgroundImage2 from "../../../public/media/headerImage2.jpg";
+// import backgroundImage3 from "../../../public/media/headerImage3.jpg";
 // Home page component
 export default class Home extends React.Component {
   // render
   render() {
+    // var backgroundImage1 = "../../../public/media/headerImage1.jpg";
+    // var backgroundImage2 = "../../../public/media/headerImage2.jpg";
+    var backgroundImage3 = "https://www.hospitalityinhealthcare.com/wp-content/uploads/2017/03/1-WELCOME-IMAGE_medical-personnel-consult.jpg";
     return (
-      <div className="page-home">
+      <div>
         <Header/>
-        <header id="home-section">
-          <div className="dark-overlay">
-            <div className="home-inner container">
-              <div className="row">
-                <div className="col-lg-8 d-none d-lg-block">
-                  <h1 className="display-4">Build
-                    <strong>social profiles</strong> and gain revenue
-                    <strong>profits</strong>
-                  </h1>
-                  <div className="d-flex">
-                    <div className="p-4 align-self-start">
-                      <i className="fas fa-check fa-2x"></i>
+        <header id="home-section" style = {{ "background-image": `url(${backgroundImage3})`}}>
+            <div className="dark-overlay">
+              <div className="home-inner container">
+                <div className="row">
+                  <div className="col-lg-8 d-none d-lg-block">
+                    <h1 className="display-4">Build
+                      <strong>social profiles</strong> and gain revenue
+                      <strong>profits</strong>
+                    </h1>
+                    <div className="d-flex">
+                      <div className="p-4 align-self-start">
+                        <i className="fas fa-check fa-2x"></i>
+                      </div>
+                      <div className="p-4 align-self-end">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
+                      </div>
                     </div>
-                    <div className="p-4 align-self-end">
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
+
+                    <div className="d-flex">
+                      <div className="p-4 align-self-start">
+                        <i className="fas fa-check fa-2x"></i>
+                      </div>
+                      <div className="p-4 align-self-end">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
+                      </div>
+                    </div>
+
+                    <div className="d-flex">
+                      <div className="p-4 align-self-start">
+                        <i className="fas fa-check fa-2x"></i>
+                      </div>
+                      <div className="p-4 align-self-end">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
+                      </div>
                     </div>
                   </div>
 
-                  <div className="d-flex">
-                    <div className="p-4 align-self-start">
-                      <i className="fas fa-check fa-2x"></i>
-                    </div>
-                    <div className="p-4 align-self-end">
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
-                    </div>
-                  </div>
-
-                  <div className="d-flex">
-                    <div className="p-4 align-self-start">
-                      <i className="fas fa-check fa-2x"></i>
-                    </div>
-                    <div className="p-4 align-self-end">
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, tempore iusto in minima facere dolorem!
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-lg-4">
-                  <div className="card bg-primary text-center card-form">
-                    <div className="card-body">
-                      <h3>Sign Up Today</h3>
-                      <p>Please fill out this form to register</p>
-                      <form>
-                        <div className="form-group">
-                          <input type="text" className="form-control form-control-lg" placeholder="Username"/>
-                        </div>
-                        <div className="form-group">
-                          <input type="email" className="form-control form-control-lg" placeholder="Email"/>
-                        </div>
-                        <div className="form-group">
-                          <input type="password" className="form-control form-control-lg" placeholder="Password"/>
-                        </div>
-                        <div className="form-group">
-                          <input type="password" className="form-control form-control-lg" placeholder="Confirm Password"/>
-                        </div>
-                        <input type="submit" value="Submit" className="btn btn-outline-light btn-block"/>
-                      </form>
+                  <div className="col-lg-4">
+                    <div className="card bg-primary text-center card-form">
+                      <div className="card-body">
+                        <h3>Sign Up Today</h3>
+                        <p>Please fill out this form to register</p>
+                        <form>
+                          <div className="form-group">
+                            <input type="email" className="form-control form-control-lg" placeholder="Email"/>
+                          </div>
+                          <div className="form-group">
+                            <input type="password" className="form-control form-control-lg" placeholder="Password"/>
+                          </div>
+                          <div className="form-group">
+                            <input type="password" className="form-control form-control-lg" placeholder="Confirm Password"/>
+                          </div>
+                          <input type="submit" value="Submit" className="btn btn-outline-light"/>
+                        </form>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </header>
-      </div>
+          </header>
+        </div>
     );
   }
 }
